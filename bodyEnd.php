9820767c8f7a73cc7e62ee014a04dd49b38de04b
DESKTOP-2EIQKKH\matt <matt@mattbauman.com>
2018-08-25 20:58:34 -0500

    State fair post

--- bodyEnd.php
+++ bodyEnd.php
@@ -21,6 +21,7 @@
 <div class="w3-container" id="blog_list" style="padding-bottom:32px;">
   <div class="w3-content w3-card w3-padding">
     <h1 class="w3-center"><b>BLOG</b></h1>
+    <p><a href="/2018-08-22_state_fair/">2018-08-22 State Fair</a></p>
     <p><a href="/2018-08-04_nisswa/">2018-08-04 Nisswa, MN</a></p>
     <p><a href="/2018-05-21_red_wing/">2018-05-21 Red Wing, MN</a></p>
     <p><a href="/2018-03-26_lacrosse/">2018-03-26 Weekend Adventures in Wisconsin</a></p>
